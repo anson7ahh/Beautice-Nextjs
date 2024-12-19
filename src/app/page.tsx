6b90a0dd6596af9c28408da3e52fa459a23dc56b
anson7ahh <nguyenanson554@gmail.com>
2024-12-19 10:56:36 +0700

    feat: check deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import AboutUsService from "../components/AboutUsService";
-import ContactUs from "../components/contactUs";
-import Footer from "../components/footer";
-import { Header } from "../components/header";
-import MainService from "../components/mainService";
-import Professional from "../components/professional";
+import AboutUsService from "@/components/AboutUsService";
+import ContactUs from "@/components/contactUs";
+import Footer from "@/components/footer";
+import { Header } from "@/components/header";
+import MainService from "@/components/mainService";
+import Professional from "@/components/professional";
 
 export default function Home() {
   return (
